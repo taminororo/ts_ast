324ab93df888f8ec72b00f2eee6d5fe6f9dedeeb
taminororo <169162271+taminororo@users.noreply.github.com>
2026-01-12 23:31:06 +0900

    feat: Publish a linter package to npm

--- my-ast/003-analyze-ast/__mocks__/inValidInput.ts
+++ my-ast/003-analyze-ast/__mocks__/inValidInput.ts
@@ -1,55 +1,59 @@
 class Animal {
     species: string;
-
+   
     constructor(species: string) {
-        this.species = species;
+      this.species = species;
     }
-
+   
     showSpecies() {
-        console.log(`この動物は ${this.species} です。`);
+      console.log(`この動物は ${this.species} です。`);
     }
-
+   
     eat(food: string) {
-        console.log(`${this.species}は${food}を食べています．`);
+      console.log(`${this.species} は ${food} を食べています。`);
     }
-
+   
     sleep(hours: number) {
-        console.log(`${this.species}は${hours}時間眠ります．`);
+      console.log(`${this.species} は ${hours} 時間眠ります。`);
     }
-
+   
     move(distance: number) {
-        console.log(`${this.species}は${distance}メートル移動しました．`);
+      console.log(`${this.species} は ${distance} メートル移動しました。`);
     }
-}
-
-class Dog extends Animal {
-    name:string;
+  }
+   
+  class Dog extends Animal {
+    name: string;
     age: number;
-
+   
     constructor(
-        name: string,
-        age: number
+      name: string,
+      age: number
     ) {
-        
-
-        this.name = name;
-        this.age = age;
+      this.name = name;
+      this.age = age;
     }
-
+   
     showInfo() {
-        console.log(`名前: ${this.name}, 年齢: ${this.age}`);
+      console.log(`名前: ${this.name}, 年齢: ${this.age}`);
     }
-
+   
     bark() {
-        console.log(`${this.name} が吠えました: ワンワン！`);
+      console.log(`${this.name} が吠えました: ワンワン！`);
     }
-}
-
-const pochi = new Dog('ポチ', 3);
-
-pochi.showSpecies();
-pochi.eat('ドッグフード');
-pochi.sleep(8);
-pochi.move(10);
-pochi.showInfo();
-pochi.bark();
+   
+    // 追加: 開始
+    noThisMethod() {
+      console.log('This method should not be called.');
+    }
+    // 追加: 終了
+  }
+   
+  const pochi = new Dog('ポチ', 3);
+  pochi.showSpecies();                // この動物は Dog です。
+  pochi.eat('ドッグフード');          // Dog は ドッグフード を食べています。
+  pochi.sleep(8);                     // Dog は 8 時間眠ります。
+  pochi.move(10);                     // Dog は 10 メートル移動しました。
+  pochi.showInfo();                   // 名前: ポチ, 年齢: 3
+  pochi.bark();                       // ポチ が吠えました: ワンワン！
+  
