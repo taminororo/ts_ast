e156259a8630264fbba2a0d0a6cf9d4f89691e55
taminororo <169162271+taminororo@users.noreply.github.com>
2026-01-12 21:51:52 +0900

    feat: define Dog class inheriting from Animal and create instance

---
+++ my-ast/003-analyze-ast/__mocks__/validInput.ts
@@ -0,0 +1,55 @@
+class Animal {
+    species: string;
+
+    constructor(species: string) {
+        this.species = species;
+    }
+
+    showSpecies() {
+        console.log(`この動物は ${this.species} です。`);
+    }
+
+    eat(food: string) {
+        console.log(`${this.species}は${food}を食べています．`);
+    }
+
+    sleep(hours: number) {
+        console.log(`${this.species}は${hours}時間眠ります．`);
+    }
+
+    move(distance: number) {
+        console.log(`${this.species}は${distance}メートル移動しました．`);
+    }
+}
+
+class Dog extends Animal {
+    name:string;
+    age: number;
+
+    constructor(
+        name: string,
+        age: number
+    ) {
+        super('Dog');
+
+        this.name = name;
+        this.age = age;
+    }
+
+    showInfo() {
+        console.log(`名前: ${this.name}, 年齢: ${this.age}`);
+    }
+
+    bark() {
+        console.log(`${this.name} が吠えました: ワンワン！`);
+    }
+}
+
+const pochi = new Dog('ポチ', 3);
+
+pochi.showSpecies();
+pochi.eat('ドッグフード');
+pochi.sleep(8);
+pochi.move(10);
+pochi.showInfo();
+pochi.bark();
---
+++ my-ast/003-analyze-ast/console.ts
@@ -0,0 +1,22 @@
+export function error(message: string): void {
+    console.error(`[ERROR] ${message}`);
+  }
+   
+  export function info(message: string): void {
+    console.log(`[INFO] ${message}`);
+  }
+   
+  export function warn(message: string): void {
+    console.warn(`[WARN] ${message}`);
+  }
+   
+  export function success(message: string): void {
+    console.log(`[SUCCESS] ${message}`);
+  }
+   
+  export function debug(message: string): void {
+    if (process.env.DEBUG === 'true') {
+      console.log(`[DEBUG] ${message}`);
+    }
+  }
+  
